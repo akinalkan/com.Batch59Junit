6e240128a2b0ffbc283eade4a6b9f6038d25f52c
Akin Alkan <akinalkan45@gmail.com>
2022-10-02 03:24:10 +0300

    day14

--- src/test/java/day14_Excel/C03_ReadExcel.java
+++ src/test/java/day14_Excel/C03_ReadExcel.java
@@ -1,2 +1,41 @@
-package day14_Excel;public class C03_ReadExcel {
+package day14_Excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C03_ReadExcel {
+    @Test
+    public void readExceltesti() throws IOException {
+        //input olarak verilen
+        //satirNo,sutunNo degerlerini parametre olarakalip
+        //O cell'deki datayi String olarak bana donduren bir method olusturun
+        int satirNo=12;
+        int sutunNo=2;
+
+        //Donen String'inCezayir oldugunu test edin
+        String expectedata="Baku";
+
+
+       String actualData= banaDataGetir(satirNo-1,sutunNo-1);//Excel index ile calistigi icin 1 cikarildi
+        Assert.assertEquals(expectedata,actualData);
+
+    }
+
+    public static String banaDataGetir(int satirIndex, int sutunIndex) throws IOException {
+     String istenenData="";
+     String dosyaYolu="src/resources/ulkeler.xlsx";
+        FileInputStream fis=new FileInputStream(dosyaYolu);
+        Workbook workbook= WorkbookFactory.create(fis);
+        istenenData=workbook.getSheet("Sayfa1")
+                .getRow(satirIndex)
+                .getCell(sutunIndex)
+                .toString();
+     return istenenData;
+    }
 }
--- src/test/java/day14_Excel/C04_ReadExcel.java
+++ src/test/java/day14_Excel/C04_ReadExcel.java
@@ -1,2 +1,36 @@
-package day14_Excel;public class C04_ReadExcel {
+package day14_Excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class C04_ReadExcel {
+    @Test
+    public void readExcelTest() throws IOException {
+
+        //7. Dosya yolunu bir String degiskene atayalim
+        String dosyaYolu = "src/resources/ulkeler.xlsx";
+        //8. FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
+        FileInputStream fis = new FileInputStream(dosyaYolu);
+        //9. Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
+        //10. WorkbookFactory.create(fileInputStream)
+        Workbook workbook = WorkbookFactory.create(fis);
+        //Ingilizce baskentler sutununu yazdiriniz
+        int sonSatir = workbook.getSheet("Sayfa1").getLastRowNum();//190-->index
+        String satirdakiData = "";
+        List<String> baskentler=new ArrayList<>();
+
+        for (int i = 0; i <= sonSatir - 1; i++) {
+            satirdakiData = C03_ReadExcel.banaDataGetir(i, 1);
+            baskentler.add(satirdakiData);
+        }
+        System.out.println(baskentler);
+
+    }
 }
